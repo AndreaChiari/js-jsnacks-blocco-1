6c774a307a83ab3da12463bca56456df6cffdefb
AndreaChiari <chiariandrea94@gmail.com>
2022-12-13 17:21:33 +0100

    update

--- snack1.js
+++ snack1.js
@@ -4,15 +4,44 @@
 */
 
 
-for (i=0; i < 10; i++){
-    const numbers = prompt("inserisci un numero");
-    console.log(numbers);
+const result = document.getElementById('sum');
+let total = 0;
+for (i=0; i < 3; i++){
+    const numbers = parseInt(prompt("inserisci un numero"));
+    console.log(typeof numbers);
+    
 
-   if(isNaN(numbers)){
-    alert('You must use a number!');
+    if(isNaN(numbers)){
+        alert('You must use a number!');
     }
-
-
-   
-   
+    else{total = total + numbers;
+        console.log(total);
+    }
+    
 }
+
+result.innerText = (total);
+/*
+
+___________
+i = 0
+total = 0;
+number = 5
+___________
+
+___________
+i = 1
+total = 5;
+number = 3
+___________
+
+___________
+i = 2
+total = 8;
+number = 4
+___________
+
+
+
+*/
+
